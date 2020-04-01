3f0a9999f709b1d107dd4f818f26cc8b6d2277e1
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-01 18:40:19 -0500

    Add files via upload

--- net-acl.tf
+++ net-acl.tf
@@ -1,49 +1,49 @@
 resource "ibm_is_network_acl" "default_acl" {
   name = "${var.vpc_name}-default-acl"
 
-  rules = [
-    {
+  rules {
       name        = "${var.vpc_name}-default-deny-all-ingress"
       action      = "deny"
       source      = "0.0.0.0/0"
       destination = "0.0.0.0/0"
       direction   = "inbound"
-    },
-    {
+    }
+    
+    rules {
       name        = "${var.vpc_name}-default-deny-all-egress"
       action      = "deny"
       source      = "0.0.0.0/0"
       destination = "0.0.0.0/0"
       direction   = "outbound"
-    },
-  ]
+    }
 }
 
 resource "ibm_is_network_acl" "webapptier_acl" {
   name = "${var.vpc_name}-webapptier-acl"
 
-  rules = [
-    {
+  rules {
       name      = "${var.vpc_name}-webapptier-icmp-all"
       direction = "inbound"
       action    = "allow"
       source    = "0.0.0.0/0"
 
       icmp = {
-        type = "0"
-        code = "0"
+        type = 0
+        code = 0
       }
 
       destination = "0.0.0.0/0"
-    },
-    {
+    }
+
+    rules {
       name        = "${var.vpc_name}-webapptier-within-vpc"
       direction   = "inbound"
       action      = "allow"
       source      = "${var.address-prefix-vpc}"
       destination = "${var.address-prefix-vpc}"
-    },
-    {
+    }
+
+    rules {
       name        = "${var.vpc_name}-webapptier-web-http-traffic"
       direction   = "inbound"
       action      = "allow"
@@ -54,13 +54,12 @@ resource "ibm_is_network_acl" "webapptier_acl" {
         port_min = "80"
         port_max = "80"
       }
-    },
-    {
+    }
+    rules {
       name        = "${var.vpc_name}-webapptier-allow-all-egress"
       action      = "allow"
       source      = "0.0.0.0/0"
       destination = "0.0.0.0/0"
       direction   = "outbound"
-    },
-  ]
+    }
 }
