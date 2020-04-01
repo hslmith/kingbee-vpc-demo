6c02a6a4b5f1a944c0688292230884423560b1e8
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-01 14:56:32 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "ibm_is_instance" "web-instancez01" {
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone1}"
   keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
-  user_data = file("web_a.cfg")
+  user_data = file("web_a.cfg"
 }
 
 
@@ -182,5 +182,5 @@ resource "ibm_is_instance" "web-instancez02" {
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone2}"
   keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
-  user_data = file("web_b.cfg")
+  user_data = "${file("web_b.cfg")}"
 }
