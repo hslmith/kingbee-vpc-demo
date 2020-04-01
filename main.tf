286cf29c098bfffc31ee0aba31d805bcec783d29
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-01 13:42:38 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -14,10 +14,14 @@ data "ibm_is_ssh_key" "sshkey1" {
 //Create VPC
 ///////////////
 
+data "ibm_resource_group" "resource" {
+  name = "${var.resource_group}"
+}
+
 resource "ibm_is_vpc" "vpc1" {
   name = "${var.vpc_name}"
   address_prefix_management = "manual"
-  resource_group = "${var.resource_group.id}"
+  resource_group = "${data.ibm_resource_group.resource.id}"
 }
 
 ////////////////
