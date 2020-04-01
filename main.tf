14805ae8ef05492ae9b50467bc220d6a48ce8b35
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-01 16:29:32 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -136,6 +136,7 @@ resource "ibm_is_instance" "web-instancez01" {
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone1}"
   keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  resource_group = "${data.ibm_resource_group.resource.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-web-a.rendered}"
 }
 
@@ -180,6 +181,7 @@ resource "ibm_is_instance" "web-instancez02" {
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone2}"
   keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
-  user_data = "${data.template_cloudinit_config.cloud-init-web-a.rendered}"
+  resource_group = "${data.ibm_resource_group.resource.id}"
+  user_data = "${data.template_cloudinit_config.cloud-init-web-b.rendered}"
   //user_data = "${file("${path.module}/web_b.cfg")}"
 }
