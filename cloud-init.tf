3189fa38dd93fd0c802bf92d2b466131909106aa
hslmith <kyrell@gmail.com>
2020-04-02 14:13:58 -0500

    UPDATE

--- cloud-init.tf
+++ cloud-init.tf
@@ -16,10 +16,12 @@ data "template_cloudinit_config" "cloud-init-left" {
   gzip            = false
   base64_encode   = false
 
+  part {
     filename      = "init.cfg"
     content_type  = "text/cloud-config"
     content       = "${data.local_file.cloud-config-web-left-txt.content}"
   }
+}
 
 
 
@@ -27,9 +29,11 @@ data "template_cloudinit_config" "cloud-init-right" {
   gzip            = false
   base64_encode   = false
 
+  part {
     filename      = "init.cfg"
     content_type  = "text/cloud-config"
     content       = "${data.local_file.cloud-config-web-right-txt.content}"
   }
+}
 
   
