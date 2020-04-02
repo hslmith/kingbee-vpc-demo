ab4b623d61a61afd5e97417d030e0165a68172a5
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-01 20:56:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "ibm_is_subnet" "websubnet1" {
   depends_on      = ["ibm_is_vpc_address_prefix.prefix_z1"]
 }
 
-/*
+
 //--- Web Server(s)
 
 resource "ibm_is_instance" "web-instancez01" {
@@ -139,7 +139,7 @@ resource "ibm_is_instance" "web-instancez01" {
   resource_group = "${data.ibm_resource_group.resource.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-web-a.rendered}"
 }
-*/
+
 
 /////////////////////
 //   ZONE 2 (RIGHT)
@@ -164,7 +164,7 @@ resource "ibm_is_subnet" "websubnet2" {
   depends_on      = ["ibm_is_vpc_address_prefix.prefix_z2"]
 }
 
-/*
+
 
 //--- Web Server(s)
 
@@ -183,6 +183,5 @@ resource "ibm_is_instance" "web-instancez02" {
   keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
   resource_group = "${data.ibm_resource_group.resource.id}"
   user_data = "${data.template_cloudinit_config.cloud-init-web-b.rendered}"
-  //user_data = "${file("${path.module}/web_b.cfg")}"
 }
-*/
+
