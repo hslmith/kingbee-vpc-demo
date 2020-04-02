c29dcefc79d1b1cb86c0c44a26c50cb89de61365
hslmith <47323378+hslmith@users.noreply.github.com>
2020-04-02 10:36:44 -0500

    Add files via upload

---
+++ net-acl.tf
@@ -0,0 +1,47 @@
+resource "ibm_is_network_acl" "isWebServerACL" {
+  name = "${var.vpc_name}-webserver-acl"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  
+  rules {
+    name        = "outbound"
+    action      = "allow"
+    source      = "0.0.0.0/0"
+    destination = "0.0.0.0/0"
+    direction   = "outbound"
+    tcp {
+      port_max        = 65535
+      port_min        = 1
+      source_port_max = 60000
+      source_port_min = 22
+    }
+  }
+  rules {
+    name        = "inbound"
+    action      = "allow"
+    source      = "0.0.0.0/0"
+    destination = "${var.address-prefix-vpc}"
+    direction   = "inbound"
+    tcp {
+      port_max        = 80
+      port_min        = 80
+      source_port_max = 60000
+      source_port_min = 22
+    }
+  }
+
+
+    rules {
+    name        = "inbound"
+    action      = "allow"
+    source      = "0.0.0.0/0"
+    destination = "${var.address-prefix-vpc}"
+    direction   = "inbound"
+    tcp {
+      port_max        = 22
+      port_min        = 22
+      source_port_max = 60000
+      source_port_min = 22
+    }
+
+  }
+}
